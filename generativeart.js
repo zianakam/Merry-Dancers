777962fe729b8bb0c0721fc13ad053b479af15ca
Ziana <zkam215@hotmail.com>
2021-12-08 23:25:26 -0800

    random generation of spine values

--- generativeart.js
+++ generativeart.js
@@ -1,27 +1,22 @@
-var spineX;
-var spineY;
 var spinePts = [];
 var k = 0;
 var j = 2;
 var val = 0;
 var inc;
+var arc;
+
+//Line not starting in mid anymore? Something to do w/var vs let?
 
 //Called once when the prog starts
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background('#222222');
 
-    spineX = 0;
-    spineY = height / 2;
-    var ty = 5000;
+    let frequency = randomGaussian(55, 20);
+    inc = TWO_PI / frequency;
+    arc = randomGaussian(40, 15);
 
-    while (spineX <= width && spineY >= 0 && spineX >= 0 && spineY <= height) {
-        spineX++;
-        spineY = map(noise(ty), 0, 1, 0, height);
-        ty += 0.002;
-        var p = createVector(spineX, spineY);
-        spinePts.push(p);
-    }
+    spinePts.generatePoints();
 }
 
 //Called directly after setup(), the draw() function continuously
@@ -35,12 +30,24 @@ var draw = function() {
         k++;
     }
 
-    inc = TWO_PI / 50;
-
     if (j == spinePts[k].x) {
         stroke('white');
-        line(spinePts[j].x, spinePts[j].y, spinePts[j].x, spinePts[j].y + sin(val) * 20);
+        line(spinePts[j].x, spinePts[j].y, spinePts[j].x, spinePts[j].y + sin(val) * arc);
         val += inc;
         j+=5;
     }
 };
+
+spinePts.generatePoints = function() {
+    let spineX = 0;
+    let spineY = height / 2;
+    let ty = 5000;
+
+    while (spineX <= width && spineY >= 0 && spineX >= 0 && spineY <= height) {
+        spineX++;
+        spineY = map(noise(ty), 0, 1, 0, height);
+        ty += 0.002;
+        var p = createVector(spineX, spineY);
+        spinePts.push(p);
+    }
+}
